d702a007973734e766fd17e13c968b0d37e9735e
AlessandroOS <alessandro0014@hotmail.com>
2022-07-05 11:53:42 -0300

    Commit final

---
+++ src/main/java/com/alessandro/SpringbootAndReactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alessandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndReactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAndReactBackendApplication.class, args);
+	}
+
+}
